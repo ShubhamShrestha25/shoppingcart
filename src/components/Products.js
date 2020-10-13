597605318bd0ebb4e9d55bbbe5378fb7d8c4152d
Shubham Shrestha <shubhamshrestha25@gmail.com>
2020-10-13 18:31:15 +0545

    redux in products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,97 +1,108 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 import Modal from "react-modal";
 import Zoom from "react-reveal/Zoom";
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions/productActions";
 
- class Products extends Component {
-   constructor(props) {
-     super(props)
-   
-     this.state = {
-        product: null,
-     }
-   }
-   openModal = (product) => {
-     this.setState({product});
-   }
-   closeModal = () => {
-     this.setState({product: null})
-   }
+class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: null,
+    };
+  }
+  componentDidMount() {
+    this.props.fetchProducts();
+  }
+
+  openModal = (product) => {
+    this.setState({ product });
+  };
+  closeModal = () => {
+    this.setState({ product: null });
+  };
 
   render() {
-    const {product} = this.state;
+    const { product } = this.state;
     return (
       <div>
-      <Fade buttom cascade>
-      <ul className="products">
-        {this.props.products.map((product) => (
-          <li key={product._id}>
-            <div className="product">
-              <a
-                href={"#" + product._id}
-                onClick={() => this.openModal(product)}
-              >
-                <img  src={product.image} alt={product.title}></img>
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div> {formatCurrency(product.price)}</div>
-                <button
-                  onClick={() => this.props.addToCart(product)}
-                  className="button primary"
-                >
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-      </Fade>
-      {
-        product && (
-          <Modal
-          isOpen={true}
-          onRequestClose={this.closeModal}
-          >
-            <Zoom>
-              <button className="close-modal" onClick={this.closeModal}>x</button>
-              <div className="product-details">
-              <img  src={product.image} alt={product.title}></img>
-              <div className="product-details-description">
-                <p>
-                  <strong>{product.title}</strong>
-                </p>
-                <p>
-                  {product.description}
-                </p>
-                <p>
-                  Avaiable Sizes: {" "}
-                  {product.availableSizes.map(x => (
-                    <span> {" "} <button className="button">{x}</button></span>
-                  ))}
-                </p>
-                <div className="product-price">
-                  <div>
-                    {formatCurrency(product.price)}
+        <Fade bottom cascade>
+          {!this.props.products ? (
+            <div>Loading...</div>
+          ) : (
+            <ul className="products">
+              {this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <a
+                      href={"#" + product._id}
+                      onClick={() => this.openModal(product)}
+                    >
+                      <img src={product.image} alt={product.title}></img>
+                      <p>{product.title}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{formatCurrency(product.price)}</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        Add To Cart
+                      </button>
+                    </div>
                   </div>
-                  <button className="button primary" onClick={() =>{
-                    this.props.addToCart(product)
-                    this.closeModal();
-                  }}>
-                    Add to Cart
-                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Fade>
+        {product && (
+          <Modal isOpen={true} onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+              </button>
+              <div className="product-details">
+                <img src={product.image} alt={product.title}></img>
+                <div className="product-details-description">
+                  <p>
+                    <strong>{product.title}</strong>
+                  </p>
+                  <p>{product.description}</p>
+                  <p>
+                    Avaiable Sizes:{" "}
+                    {product.availableSizes.map((x) => (
+                      <span>
+                        {" "}
+                        <button className="button">{x}</button>
+                      </span>
+                    ))}
+                  </p>
+                  <div className="product-price">
+                    <div>{formatCurrency(product.price)}</div>
+                    <button
+                      className="button primary"
+                      onClick={() => {
+                        this.props.addToCart(product);
+                        this.closeModal();
+                      }}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
-              </div>
               </div>
             </Zoom>
           </Modal>
-        )
-      }
+        )}
       </div>
     );
   }
 }
 
-export default Products;
+export default connect((state) => ({ products: state.products.items }), {
+  fetchProducts,
+})(Products);
